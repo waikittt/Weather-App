bf17ce9f5cc9374ae0072d78f4bceeff4accb0c5
wkoo0004 <wkoo0004@student.monash.edu>
2023-02-14 11:20:59 +0800

    Update css, add event listener to convert temp

--- weather.js
+++ weather.js
@@ -5,6 +5,8 @@ window.addEventListener('load', () => {
     let locArea = document.querySelector('.location-area');
     let locCountry = document.querySelector('.location-country');
     let icon = document.getElementById('weather-icon')
+    let tempSection = document.querySelector('.degree-section')
+    let tempSpan = document.querySelector('.degree-section span')
     
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -21,16 +23,22 @@ window.addEventListener('load', () => {
                 .then(response => {return response.json();}) //response parsed as JSON
                 .then(data => {
                     console.log(data)
+                    const temperature = data.main.temp;
 
-                    //const {temperature, weathercode} = data.main;  // extract temp from current_weather
                     // // set DOM elements based on API
-                    tempDegree.textContent = data.main.temp;
+                    tempDegree.textContent = temperature;
                     tempDesc.textContent = data.weather[0]["description"]; //weatherDescription[weathercode];
                     locArea.textContent = data.name;
                     locCountry.textContent = data.sys.country;
 
+                    //set the icon
                     const iconID = data.weather[0]["icon"]
-                    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${iconID}@2x.png"/>`                    
+                    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${iconID}@2x.png"/>`   
+                    
+                    // change temperature to Celsius/Fahrenheit 
+                    tempSection.addEventListener('click', () => {
+                        convertTemp(temperature);
+                    });
                 });
         }); 
     }   
@@ -38,38 +46,18 @@ window.addEventListener('load', () => {
         h1.textContent = "This is not working :D"
     }
 
-    // function setIcon(icon, iconID) {
-    //     const skycons = new skycons({color: "white"});
-    //     const currentIcon = icon.replace(/ /g, "_").toUpperCase();
-    // }
+    function convertTemp (temp) {
+        if (tempSpan.textContent === "°C") {
+            tempSpan.textContent = "F";
+            let fahrenheit = (temp * 9/5) + 32;
+            tempDegree.textContent = fahrenheit.toFixed(2);
+        }
+        else {
+            tempSpan.textContent = "°C";
+            tempDegree.textContent = temp;
+        }
+    }
 });
 
-// Weather interpretation codes 
-// const weatherDescription = {
-//     "0" : "Clear Sky",
-//     "1" : "Mainly Clear",
-//     "2" : "Partly Cloudy",
-//     "3" : "Overcast",
-//     "45" : "Fog",
-//     "48" : "Depositing Rime Fog",
-//     "51" : "Light Drizzle",
-//     "53" : "Moderate Drizzle",
-//     "55" : "Dense Intensity Drizzle",
-//     "56" : "Light Freezing Drizzle", 
-//     "57" : "Dense Intensity Freezing Drizzle",
-//     "61" : "Slight Rain",
-//     "63" : "Moderate Rate",
-//     "65" : "Heavy Intensity Rain",
-//     "66" : "Light Freezing Rain",
-//     "67" : "Heavy Intensity Freezing Rain",
-//     "71" : "Slight Snow Fall",
-//     "73" : "Moderate Snow Fall",
-//     "75" : "Heavy Intensity Snow Fall",
-//     "77" : "Snow Grains",
-//     "80" : "Slight Rain Showers",
-//     "81" : "Moderate Rain Showers",
-//     "82" : "Violent Rain Showers",
-//     "85" : "Slight Snow Showers",
-//     "86" : "Heavy Snow Showers", 
-//     "95" : "Thunderstorm"
-// }
+
+
